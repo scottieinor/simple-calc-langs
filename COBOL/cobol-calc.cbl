5d2285fbbb4cf572f27459a1eade155703f9884a
Scott <scootys@gmail.com>
2014-07-28 15:50:41 -0700

    Calculator Calculates some issues.
    The calc will do simple math, add, subtract, multiply and divide.
    The results are displayed with leading zeros.
    Looks like moving the variables to display variables may correct the issue.

--- COBOL/cobol-calc.cbl
+++ COBOL/cobol-calc.cbl
@@ -7,34 +7,36 @@
        OBJECT-COMPUTER. scootys-home.
 
        DATA DIVISION.
-       DECLARE-VARS SECTION.
-       05 firstnum  PIC 9(4).
-       05 operator  PIC X.
-       05 secondnum PIC 9(4).
+       WORKING-STORAGE SECTION.
+       01 firstnum  PIC 9(4) USAGE PACKED-DECIMAL.
+       01 oper      PIC X(1).
+       01 secondnum PIC 9(4) USAGE PACKED-DECIMAL.
+       01 total     PIC 9(8) USAGE DISPLAY.
 
        PROCEDURE DIVISION.
 
        BEGIN.
-           DISPLAY "Enter first number".
-           ACCEPT firstnum FROM COMMAND-LINE.
-           DISPLAY "Enter operator (+ - * /)".
-           ACCEPT operator FROM COMMAND-LINE.
-           DISPLAY "Enter second number".
-           ACCEPT secondnum FROM COMMAND-LINE.
+       ACCEPT firstnum  FROM ARGUMENT-VALUE.
+       ACCEPT oper      FROM ARGUMENT-VALUE.
+       ACCEPT secondnum FROM ARGUMENT-VALUE.
 
-           IF operator = '+' THEN
-               COMPUTE total = firstnum + secondnum.
-           ELSE
-               IF operator = '-' THEN
-                   COMPUTE total = firstnum - secondnum.
-           ELSE
-               IF operator = '*' THEN
-                   COMPUTE total = firstnum * secondnum.
-           ELSE
-               IF operator = '/' THEN
-                      COMPUTE total = firstnum / secondnum.
-           ELSE
-                  DISPLAY "Sorry I do not know that operator".
-           END-IF.
+       DISPLAY firstnum SPACE oper SPACE secondnum.
 
-           STOP RUN.
+       IF oper = '+' THEN
+           COMPUTE total = firstnum + secondnum
+       ELSE
+           IF oper = "-" THEN
+                  COMPUTE total = firstnum - secondnum
+       ELSE
+           IF oper = "*" THEN
+                  COMPUTE total = firstnum * secondnum
+       ELSE
+           IF oper = "/" THEN
+                  COMPUTE total = firstnum / secondnum
+       ELSE
+           DISPLAY "Sorry I do not know that operator"
+       END-IF.
+
+       DISPLAY 'The total is: ' total.
+
+       STOP RUN.
